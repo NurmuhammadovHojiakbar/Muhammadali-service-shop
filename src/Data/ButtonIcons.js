7b91029a767a9847f0befe496162aec9a92ffeb8
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2022-01-12 21:13:00 +0500

    delete unused icons

--- src/Data/ButtonIcons.js
+++ src/Data/ButtonIcons.js
@@ -1,5 +1,3 @@
-import MailIcon from "@mui/icons-material/Mail"
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export const ButtonIconsList = [
